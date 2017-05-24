88cb78c70b38bc2f7c6230767f9cdc1722148314
Elizabeth Flournoy <bflournoy@gmail.com>
2017-05-24 10:04:40 -0600

    problem 5 done

--- run.js
+++ run.js
@@ -1,13 +1,27 @@
-//PROBLEM 4
+//PROBLEM 5
 var fs = require('fs');
-var file = fs.readFile(process.argv[2], 'utf8', function callback(err, data) {
-  if (err){
-    return 'error';
+var path = require('path');
+fs.readdir(process.argv[2], function callback(err, arr){
+  if (err){return 'error';}
+  var extFilter = "."+process.argv[3]
+  for (let i=0; i<arr.length; i++){
+    let test = path.extname(arr[i])
+    if (test === extFilter){
+      console.log(arr[i]);
+    }
   }
-  var strings = data.toString().split('\n')
-  console.log(strings.length-1);
-  });
+})
 
+
+//PROBLEM 4
+// var fs = require('fs');
+// var file = fs.readFile(process.argv[2], 'utf8', function callback(err, data) {
+//   if (err){
+//     return 'error';
+//   }
+//   var strings = data.toString().split('\n')
+//   console.log(strings.length-1);
+//   });
 
 
 //PROBLEM 3
