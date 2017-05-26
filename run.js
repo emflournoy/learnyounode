2f450b69f96ce482fb327e4d685f4371cd98ad6c
Elizabeth Flournoy <bflournoy@gmail.com>
2017-05-26 16:06:56 -0600

    completed 8

--- run.js
+++ run.js
@@ -1,3 +1,13 @@
+//PROBLEM 8
+var bl = require('bl');
+const http = require('http');
+var link = process.argv[2];
+http.get(link, function callback(response){
+  response.pipe(bl(function (err, data) {
+    console.log(data.length);
+    console.log(data.toString());
+  }))
+})
 
 
 //PROBLEM 7
